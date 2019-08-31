18046854aed56926f18e1f431b434a174eea8c5b
Panela Leung <leungpanela@gmail.com>
2019-08-31 14:17:31 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,21 @@
 # Make your shoe class here!
+class shoe
+
+
+  def initialize (brand)
+    @brand = brand
+  end
+
+  def color (color)
+    @color = color
+  end
+  #this is the setter
+  def color=(color)
+    @color = color
+  end
+  #this is the getter
+  def color
+    @color
+  end
+
+end
