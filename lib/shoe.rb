b824b6f872ac46e344396e01ce06b796b9397d6c
Panela Leung <leungpanela@gmail.com>
2019-08-31 14:55:46 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -54,14 +54,14 @@ class Shoe
     @material
   end
 
-  def condition (condition)
-    @condition = condition
-  end
-
-  # def condition= (condition)
+  # def condition (condition)
   #   @condition = condition
   # end
-  #
+
+  def condition= (condition)
+    @condition = condition
+  end
+  
   # def condition
   #   @condition
   # end
