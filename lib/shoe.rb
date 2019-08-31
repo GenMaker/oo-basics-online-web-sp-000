43abb206875413f1a7852253b76cf1cb297e06d4
Panela Leung <leungpanela@gmail.com>
2019-08-31 14:24:55 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,20 +1,20 @@
 # Make your shoe class here!
 class Shoe
-  attr_accessor = 
+  attr_accessor = :color, :brand, :size,:material, :condition
   def initialize (brand)
     @brand = brand
   end
   #this is a reader
-  def color (color)
-    @color = color
-  end
-  #this is the setter
-  def color=(color)
-    @color = color
-  end
-  #this is the getter
-  def color
-    @color
-  end
+  # def color (color)
+  #   @color = color
+  # end
+  # #this is the setter
+  # def color=(color)
+  #   @color = color
+  # end
+  # #this is the getter
+  # def color
+  #   @color
+  # end
 
 end
